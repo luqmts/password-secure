dfba95ab2693b63c5e4c1b07b7c10f9e9d2bb758
lucas mateus <lucas.mts1812@gmail.com>
2025-08-19 22:37:02 -0300

    Removed a redudant brackets on RequestMapping

--- src/main/java/com/luq/secure_password/controllers/PasswordController.java
+++ src/main/java/com/luq/secure_password/controllers/PasswordController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(("/validate-password"))
+@RequestMapping("/validate-password")
 public class PasswordController {
     @Autowired
     PasswordService passwordService;
